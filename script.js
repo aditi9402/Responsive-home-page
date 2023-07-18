81fb4b65203bfa8340135d608baefa0aeeedc649
aditi9402 <114405540+aditi9402@users.noreply.github.com>
2023-07-19 04:00:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function menuToggle(){
+    const toggleMenu=document.querySelector('.toggleMenu');
+    const navigation=document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+let wave1=document.getElementById('wave1');
+let wave2=document.getElementById('wave2');
+let wave3=document.getElementById('wave3');
+let wave4=document.getElementById('wave4');
+
+window.addEventListener('scroll',function(){
+    let value=window.scrollY;
+
+    wave1.style.backgroundPositionX=400+value*4+'px';
+    wave2.style.backgroundPositionX=300+value*-4+'px';
+    wave3.style.backgroundPositionX=200+value*2+'px';
+    wave4.style.backgroundPositionX=100+value*-2+'px';
+})
